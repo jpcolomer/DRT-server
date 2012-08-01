a6e0ca73b47929b4f30bc41a0fd3a6cf4ee5ee3c
jpcolomer <jpcolomer@gmail.com>
2012-08-01 19:14:13 -0400

    terminar de arreglar reporte send file

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -3,8 +3,8 @@ class ReportesController < ApplicationController
 	def download_pdf
 		reporte = Reporte.find(params[:id])
 		#reporte = Reporte.find_by_file_name(params[:file_name])
-		send_file("#{Rails.root}/files/reportes/mensual/1.pdf",
-			:filename => "reporte_mensual_01-07-2012.pdf",
+		send_file("#{Rails.root}/files/reportes/#{reporte.get_tipo}/#{reporte.id}.pdf",
+			:filename => "#{reporte.file_name}.pdf",
 			:type => "application/pdf")
 	end
 end
