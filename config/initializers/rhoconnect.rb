d4298acd19cadf685e6c9ba8c7fe8e57a9bc37dd
jpcolomer <jpcolomer@gmail.com>
2013-03-12 16:49:45 -0300

    change app url

--- config/initializers/rhoconnect.rb
+++ config/initializers/rhoconnect.rb
@@ -1,4 +1,4 @@
 Rhoconnect.configure do |config|
-  config.app_endpoint = "http://protected-ocean-7054.herokuapp.com"
+  config.app_endpoint = "http://peaceful-cliffs-7031.herokuapp.com"
   config.authenticate = lambda { |credentials| User.authenticate(credentials['login'], credentials['password'])}
 end
